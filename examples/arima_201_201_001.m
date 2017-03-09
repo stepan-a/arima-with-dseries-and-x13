e9bfddc5e2ffc2b48d73d1cdd3fd33b7949599e1
Stéphane Adjemian (Charybdis) <stephane.adjemian@univ-lemans.fr>
2017-03-09 11:28:09 +0100

    Added two examples.

---
+++ examples/arima_201_201_001.m
@@ -0,0 +1,14 @@
+% Initialization of the endogenous variable to be simulated.
+y = dseries([0; 0], '1938Q3', 'y');
+
+% Get random draws from a Gaussian distribution.
+e = dseries(randn(201,1),'1938Q4','e');
+
+% Simulate an ARMA(2,1) model
+from 1939Q1 to 1988Q4 do y(t) = .8*y(t-1) - .2*y(t-2) + e(t) - .15*e(t-1)
+
+% Declare the ARMA model with seasonal MA component to be estimated (using X13-CENSUS)
+model = arima(y, '(2,0,1)(0,0,1)');
+
+% Trigger the estimation of the model
+estimate(model);
